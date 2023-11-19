4a4dff56043063ece288fba13ce097af90837709
qiaozha <qiaozha@microsoft.com>
2023-11-19 10:05:32 +0800

    enable-device-update-in-smoke-test

---
+++ packages/typespec-test/test/deviceupdate/generated/typespec-ts/src/index.ts
@@ -0,0 +1,11 @@
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT license.
+
+import DeviceUpdateClient from "./deviceUpdateClient";
+
+export * from "./deviceUpdateClient";
+export * from "./parameters";
+export * from "./responses";
+export * from "./clientDefinitions";
+
+export default DeviceUpdateClient;
